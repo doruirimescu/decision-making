fc8963f1e32afaa65d788d094297b8ec83e2fe89
Doru Irimescu <>
2023-07-12 14:36:06 +0300

    Add requirements and dataframe

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ from typing import ClassVar, List, Optional, Tuple, Any
 from collections import namedtuple
 from pydantic import BaseModel
 from storable import Storable
+from pandas import DataFrame as df
 
 
 ParameterData = namedtuple("ParameterData", ["name", "value", "score"])
@@ -25,6 +26,74 @@ class Dataset(Storable):
 
     def delete_data_point(self, data_point_name: str) -> None:
         self.data_points = [dp for dp in self.data_points if dp.name != data_point_name]
+
+    def dataframe(self) -> df:
+        if self.data_points is not None:
+            names = [dp.name for dp in self.data_points]
+            param_names = [p.name for p in self.data_points[0].parameter_datas]
+            param_names += ["total_score"]
+            data = [[(p.value, p.score) for p in dp.parameter_datas] for dp in self.data_points]
+            # add total score to data
+            data = [d + [dp.total_score] for d, dp in zip(data, self.data_points)]
+            return df(data, columns=param_names, index=names)
+        return df({})
+
+    def order_by_parameter_value(self, parameter_name: str, ascending: bool = True) -> df:
+        dataframe = self.dataframe()
+        dataframe.sort_values(by=[parameter_name][0], inplace=True, ascending=ascending)
+        return dataframe
+
+    def order_by_parameter_score(self, parameter_name: str, ascending: bool = True) -> df:
+        dataframe = self.dataframe()
+        dataframe.sort_values(by=['price'], inplace=True, ascending=ascending, key=lambda x: x[1])
+        return dataframe
+
 # We should also have a method to convert this to a pandas dataframe
 # We should also have a method to convert this to a numpy array
 # We should also have a method to convert this to a list of lists
+
+
+def create_test_dataset():
+    les_paul = DataPoint(
+        name="Gibson Les Paul",
+        parameter_datas=[
+            ParameterData(name="price", value=1500, score=10),
+            ParameterData(name="year", value=2016, score=0),
+            ParameterData(name="color", value="Honeyburst", score=0),
+            ParameterData(name="condition", value="As new", score=0),
+        ],
+        total_score = 10
+    )
+
+    telecaster = DataPoint(
+        name="Fender telecaster",
+        parameter_datas=[
+            ParameterData(name="price", value=1900, score=0),
+            ParameterData(name="year", value=2010, score=0),
+            ParameterData(name="color", value="Honeyburst", score=2),
+            ParameterData(name="condition", value="As new", score=3),
+        ],
+        total_score = 5
+    )
+
+    ds = Dataset(
+        name="TestDataset",
+        description="TestDataset description",
+        data_points=[les_paul, telecaster],
+    )
+    return ds
+
+
+
+# td = create_test_dataset()
+# print(td.dataframe())
+
+# dataframe = td.dataframe()
+
+# print(td.order_by_parameter_value('price'))
+
+# print(td.order_by_parameter_value('year'))
+
+# print(td.order_by_parameter_score('color'))
+
+# print(td.order_by_parameter_value('total_score'))
--- test/test_dataset.py
+++ test/test_dataset.py
@@ -4,7 +4,36 @@ import shutil
 import dataset
 import copy
 
+
 class TestDataset(unittest.TestCase):
+    def create_test_dataset(self):
+        les_paul = dataset.DataPoint(
+            name="Gibson Les Paul",
+            parameter_datas=[
+                dataset.ParameterData(name="price", value=1500, score=0),
+                dataset.ParameterData(name="year", value=2016, score=0),
+                dataset.ParameterData(name="color", value="Honeyburst", score=0),
+                dataset.ParameterData(name="condition", value="As new", score=0),
+            ],
+        )
+
+        telecaster = dataset.DataPoint(
+            name="Fender telecaster",
+            parameter_datas=[
+                dataset.ParameterData(name="price", value=1900, score=0),
+                dataset.ParameterData(name="year", value=2010, score=0),
+                dataset.ParameterData(name="color", value="Honeyburst", score=0),
+                dataset.ParameterData(name="condition", value="As new", score=0),
+            ],
+        )
+
+        ds = dataset.Dataset(
+            name="TestDataset",
+            description="TestDataset description",
+            data_points=[les_paul, telecaster],
+        )
+        return ds
+
     def test_datapoint(self):
         dp = dataset.DataPoint(
             name="TestDataset",
@@ -51,6 +80,14 @@ class TestDataset(unittest.TestCase):
         del ds
         ds = dataset.Dataset(**dataset.Dataset.load_json("TestDataset"))
         self.assertEqual(len(ds.data_points), 1)
+
+    def test_dataset_to_df(self):
+        ds = self.create_test_dataset()
+        df = ds.dataframe()
+        self.assertEqual(df.shape, (8, 3))
+        self.assertEqual(df["name"].unique().tolist(), ["price", "year", "color", "condition"])
+        print(df)
+        self.assertTrue(False)
 
     # after all tests
     @classmethod
